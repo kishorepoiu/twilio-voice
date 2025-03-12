c61af50c07a549eacef8fff2d604de445d9dbbc5
sivakumarks10 <sivakumar.k@s10health.com>
2025-03-12 11:59:53 +0530

    code udpated

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const { twiml } = require("twilio");
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 
+// Default route to check if the server is running
+app.get("/", (req, res) => {
+  res.send("Twilio Voice App is running! 🚀");
+});
+
 // Handle incoming voice calls from Twilio
 app.post("/voice", (req, res) => {
   const response = new twiml.VoiceResponse();
